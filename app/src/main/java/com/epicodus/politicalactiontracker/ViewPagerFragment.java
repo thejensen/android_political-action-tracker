45b826da786deb2e455a38ffd6b08d9008562b77
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-20 11:49:06 -0800

    Add ViewPagerFragment with its corresponding layout.

---
+++ app/src/main/java/com/epicodus/politicalactiontracker/ViewPagerFragment.java
@@ -0,0 +1,21 @@
+package com.epicodus.politicalactiontracker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jensese on 12/20/16.
+ */
+
+public class ViewPagerFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_pager, container, false);
+        return view;
+    }
+}
