f7b1b1771696430e94649498843cd51a53dad5a5
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-20 14:24:29 -0800

    Send a bundle from main activity to the view pager with the index of the item selected, so the viewpager knows which object we want. Look up fragments by tag instead of index, so the app doesn't crash in landscape view.

--- app/src/main/java/com/epicodus/politicalactiontracker/MainActivity.java
+++ app/src/main/java/com/epicodus/politicalactiontracker/MainActivity.java
@@ -4,22 +4,25 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ActionListFragment.OnActionItemSelectedInterface {
+    public static final String LIST_FRAGMENT = "list_fragment";
+    public static final String VIEW_PAGER_FRAGMENT = "view_pager_fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Create a new Fragment that displays on the Main Activity if there isn't already a Fragment created -- new fragments would be created if the user goes to landscape view
-        ActionListFragment savedFragment = (ActionListFragment) getSupportFragmentManager().findFragmentById(R.id.placeHolder);
+//        Create a new Fragment that displays on the Main Activity if there isn't already a Fragment created -- new fragments would be created if the user goes to landscape view, and find its ID by tag, rather than index
+        ActionListFragment savedFragment = (ActionListFragment) getSupportFragmentManager()
+                .findFragmentByTag(LIST_FRAGMENT);
         if (savedFragment == null) {
             ActionListFragment fragment = new ActionListFragment();
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.placeHolder, fragment);
+//            Add LIST_FRAGMENT tag because the app was looking up list fragment based on the ID of the placeholder view group which might not be holding the ID.
+            fragmentTransaction.add(R.id.placeHolder, fragment, LIST_FRAGMENT);
             fragmentTransaction.commit();
         }
     }
@@ -27,12 +30,14 @@ public class MainActivity extends AppCompatActivity implements ActionListFragmen
 //    List item selected from the actions list creates a new ViewPagerFragment which replaces the previous fragment's place in the placeholder ViewGroup in the Main Activity layout. Neat.
     @Override
     public void onListActionItemSelected(int index) {
-        Toast.makeText(MainActivity.this, Actions.names[index], Toast.LENGTH_SHORT).show();
-
         ViewPagerFragment fragment = new ViewPagerFragment();
+//        Sending the ViewPager some info!
+        Bundle bundle = new Bundle();
+        bundle.putInt(ViewPagerFragment.KEY_ACTION_INDEX, index);
+        fragment.setArguments(bundle);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.placeHolder, fragment);
+        fragmentTransaction.replace(R.id.placeHolder, fragment, VIEW_PAGER_FRAGMENT);
 //        Adding the fragment to the backstack, so the back button goes from the ViewPagerFragment to the ActivityListFragment
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
--- app/src/main/java/com/epicodus/politicalactiontracker/ViewPagerFragment.java
+++ app/src/main/java/com/epicodus/politicalactiontracker/ViewPagerFragment.java
@@ -6,15 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by jensese on 12/20/16.
  */
 
 public class ViewPagerFragment extends Fragment {
+    public static final String KEY_ACTION_INDEX = "action_index";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        Receive object in the Bundle depending on the index of the click in the list of actions
+        int index = getArguments().getInt(KEY_ACTION_INDEX);
+        Toast.makeText(getActivity(), Actions.names[index], Toast.LENGTH_SHORT).show();
+
         View view = inflater.inflate(R.layout.fragment_view_pager, container, false);
         return view;
     }
