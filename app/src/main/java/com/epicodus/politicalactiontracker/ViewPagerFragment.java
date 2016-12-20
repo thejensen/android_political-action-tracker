8d9ce4590cc25df20af91315d7cb4905bf7663dc
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-20 14:57:14 -0800

    Set the adapter for the viewPager with the childFragmentManager, and return 2 pages. Add the two fragment layouts that will represent the description and the act pages.

--- app/src/main/java/com/epicodus/politicalactiontracker/ViewPagerFragment.java
+++ app/src/main/java/com/epicodus/politicalactiontracker/ViewPagerFragment.java
@@ -3,10 +3,11 @@ package com.epicodus.politicalactiontracker;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 /**
  * Created by jensese on 12/20/16.
@@ -20,9 +21,34 @@ public class ViewPagerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        Receive object in the Bundle depending on the index of the click in the list of actions
         int index = getArguments().getInt(KEY_ACTION_INDEX);
-        Toast.makeText(getActivity(), Actions.names[index], Toast.LENGTH_SHORT).show();
-
+        getActivity().setTitle(Actions.names[index]);
         View view = inflater.inflate(R.layout.fragment_view_pager, container, false);
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+//        getChildFragmentManager is used when dealing with fragments within fragments
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return null;
+            }
+
+//            How many tabs in the pager! 2!
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
         return view;
     }
+
+//
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
+    }
+
+
+
 }
