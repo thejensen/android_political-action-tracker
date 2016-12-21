526a89589874d2fdb7f4f683a39f75e249879012
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-21 11:39:13 -0800

    Add functionality for an add action button to go to the  add action activity, which doesn't exist yet.

--- app/src/main/java/com/epicodus/politicalactiontracker/MainActivity.java
+++ app/src/main/java/com/epicodus/politicalactiontracker/MainActivity.java
@@ -1,18 +1,26 @@
 package com.epicodus.politicalactiontracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements ActionListFragment.OnActionItemSelectedInterface {
     public static final String LIST_FRAGMENT = "list_fragment";
     public static final String VIEW_PAGER_FRAGMENT = "view_pager_fragment";
+    @Bind(R.id.addActionButton) Button mAddActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
 //        Create a new Fragment that displays on the Main Activity if there isn't already a Fragment created -- new fragments would be created if the user goes to landscape view, and find its ID by tag, rather than index
         ActionListFragment savedFragment = (ActionListFragment) getSupportFragmentManager()
@@ -25,6 +33,14 @@ public class MainActivity extends AppCompatActivity implements ActionListFragmen
             fragmentTransaction.add(R.id.placeHolder, fragment, LIST_FRAGMENT);
             fragmentTransaction.commit();
         }
+
+        mAddActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddActionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 //    List item selected from the actions list creates a new ViewPagerFragment which replaces the previous fragment's place in the placeholder ViewGroup in the Main Activity layout. Neat.
@@ -42,4 +58,5 @@ public class MainActivity extends AppCompatActivity implements ActionListFragmen
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
+
 }
