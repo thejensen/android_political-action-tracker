72c848cf8f0f902125805a6ebe5d937b230229b7
Sara Jensen <jensen.sara.e@gmail.com>
2016-12-21 10:59:46 -0800

    Show details for each action on its own page.

--- app/src/main/java/com/epicodus/politicalactiontracker/ActionDescriptionFragment.java
+++ app/src/main/java/com/epicodus/politicalactiontracker/ActionDescriptionFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,8 +17,14 @@ import butterknife.ButterKnife;
  */
 
 public class ActionDescriptionFragment extends Fragment {
-    @Bind(R.id.actionImageView)
-    ImageView mActionImageView;
+    @Bind(R.id.actionImageView) ImageView mActionImageView;
+    @Bind(R.id.actionNameTextView) TextView mActionNameTextView;
+    @Bind(R.id.dateTextView) TextView mDateTextView;
+    @Bind(R.id.actionCategoryTextView) TextView mActionCategoryTextView;
+    @Bind(R.id.causeTextView) TextView mCauseTextView;
+    @Bind(R.id.linkTextView) TextView mLinkTextView;
+    @Bind(R.id.descriptionTextView) TextView mDescriptionTextView;
+
 
     @Nullable
     @Override
@@ -27,6 +34,12 @@ public class ActionDescriptionFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         mActionImageView.setImageResource(Actions.resourceIds[index]);
+        mActionNameTextView.setText(Actions.names[index]);
+        mDateTextView.setText(Actions.date[index]);
+        mActionCategoryTextView.setText(Actions.categoryAction[index]);
+        mCauseTextView.setText(Actions.categoryCause[index]);
+        mLinkTextView.setText(Actions.link[index]);
+        mDescriptionTextView.setText(Actions.description[index]);
 
         return view;
     }
